46cff28660bb9e19cd56bce6b9f69cd013dd4f7b
Simeon12345678 <simeon.milic@elev.ga.ntig.se>
2024-10-02 11:18:45 +0200

    Working hard or hardly working

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Tamagotchi tamagotchi = new Tamagotchi();
+
+        System.out.println("This is your tamagotchi: ");
+        tamagotchi.name = "Betty";
+
+        tamagotchi.teach("Hello");
+
+        tamagotchi.printStats();
     }
 }
